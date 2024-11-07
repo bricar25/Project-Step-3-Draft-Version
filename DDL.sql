391035dce764ef83dc2e6cebb54b05e40feebd62
usanro2151 <usanro2151.ds@gmail.com>
2024-11-07 17:11:49 -0600

    reformated DDL and added comments.

--- DDL.sql
+++ DDL.sql
@@ -1,3 +1,6 @@
+-- CREATE TABLES
+
+-- create Customers table
 CREATE OR REPLACE TABLE Customers (
     customerID INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     first_name VARCHAR(32) NOT NULL,
@@ -7,13 +10,7 @@ CREATE OR REPLACE TABLE Customers (
     phone_number VARCHAR(12)
 );
 
-INSERT INTO Customers (first_name, last_name, email, password, phone_num) VALUES
-('Jamie', 'Dillan', 'jdill@yahoo.com', '123pasS!', '223-505-1234'),
-('Jane', 'Smith', 'janesmith@yahoo.com', 'Pass!word2', '425-505-6678'),
-('Alice', 'James', 'alicej@gmail.com', '2p0a2s4s?', '334-758-8765'),
-('Bobby', 'Wilde', 'bwilde@gmail.com', 'beWure!', '202-577-4321');
-
-
+-- create Sellers table
 CREATE OR REPLACE TABLE Sellers (
     sellerID INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     seller_first_name VARCHAR(32) NOT NULL,
@@ -23,13 +20,7 @@ CREATE OR REPLACE TABLE Sellers (
     follower_count INT
 );
 
-INSERT INTO Sellers (seller_first_name, seller_last_name, store_name, store_rating, follower_count) VALUES
-('Pam', 'Brell', 'Winner Tech Shop', 2.8, 1023),
-('Jon', 'Wimber', 'Wimber Wear', 4.5, 850),
-('Ella', 'Garcia', 'Crate and Home', 4.9, 670),
-('Sonny', 'Smith', 'Son Shades', 3.7, 1540);
-
-
+-- create Products table
 CREATE OR REPLACE TABLE Products (
     productID INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     sellerID INT NOT NULL, 
@@ -42,14 +33,7 @@ CREATE OR REPLACE TABLE Products (
     FOREIGN KEY (sellerID) REFERENCES Sellers(sellerID)
 );
 
-INSERT INTO Products (sellerID, category, brand, size, price, product_condition, color) VALUES
-(1, 'Shirts', 'Adidas', 'Medium', 119.99, 'New', 'Black'),
-(2, 'Pants', 'Nike', 'Large', 49.99, 'New', 'Red'),
-(3, 'Pants', 'Arizona', 'Large', 19.99, 'Used', 'White'),
-(4, 'Accessories', 'Eddie Bauer', 'Small', 15.99, 'Used', 'Blue');
-
-
-
+-- create Orders table
 CREATE OR REPLACE TABLE Orders (
     orderID INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     customerID INT NOT NULL,
@@ -59,16 +43,7 @@ CREATE OR REPLACE TABLE Orders (
     FOREIGN KEY (customerID) REFERENCES Customers(customerID)
 );
 
-INSERT INTO Orders (customerID, order_date, shipped, total) VALUES
-(1, '2023-08-06', TRUE, 259.18),
-(2, '2022-11-03', TRUE, 161.97),
-(3, '2021-10-01', TRUE, 86.36),
-(4, '2024-11-07', FALSE, 86.35);
-
-
-
-
-
+-- create Customer_Seller_Relationships table. intersection table between Customers and Sellers
 CREATE OR REPLACE TABLE Customer_Seller_Relationships (
     csrID INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     customerID INT NOT NULL,
@@ -77,13 +52,7 @@ CREATE OR REPLACE TABLE Customer_Seller_Relationships (
     FOREIGN KEY (sellerID) REFERENCES Sellers(sellerID)
 );
 
-INSERT INTO Customer_Seller_Relationships (customerID, sellerID) VALUES
-(1, 1),
-(2, 2),
-(3, 3),
-(4, 4);
-
-
+-- create Line_Items table. intersection table between Orders and Products
 CREATE OR REPLACE TABLE Line_Items (
     lineitemID INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     orderID INT NOT NULL,
@@ -95,6 +64,43 @@ CREATE OR REPLACE TABLE Line_Items (
     FOREIGN KEY (productID) REFERENCES Products(productID)
 );
 
+-- insert sample data into Customers table
+INSERT INTO Customers (first_name, last_name, email, password, phone_number) VALUES
+('Jamie', 'Dillan', 'jdill@yahoo.com', '123pasS!', '223-505-1234'),
+('Jane', 'Smith', 'janesmith@yahoo.com', 'Pass!word2', '425-505-6678'),
+('Alice', 'James', 'alicej@gmail.com', '2p0a2s4s?', '334-758-8765'),
+('Bobby', 'Wilde', 'bwilde@gmail.com', 'beWure!', '202-577-4321');
+
+-- insert sample data into Sellers table
+INSERT INTO Sellers (seller_first_name, seller_last_name, store_name, store_rating, follower_count) VALUES
+('Pam', 'Brell', 'Winner Tech Shop', 2.8, 1023),
+('Jon', 'Wimber', 'Wimber Wear', 4.5, 850),
+('Ella', 'Garcia', 'Crate and Home', 4.9, 670),
+('Sonny', 'Smith', 'Son Shades', 3.7, 1540);
+
+
+-- insert sample data in Products table
+INSERT INTO Products (sellerID, category, brand, size, price, product_condition, color) VALUES
+(1, 'Shirts', 'Adidas', 'Medium', 119.99, 'New', 'Black'),
+(2, 'Pants', 'Nike', 'Large', 49.99, 'New', 'Red'),
+(3, 'Pants', 'Arizona', 'Large', 19.99, 'Used', 'White'),
+(4, 'Accessories', 'Eddie Bauer', 'Small', 15.99, 'Used', 'Blue');
+
+-- insert sample data in Orders table
+INSERT INTO Orders (customerID, order_date, shipped, total) VALUES
+(1, '2023-08-06', TRUE, 259.18),
+(2, '2022-11-03', TRUE, 161.97),
+(3, '2021-10-01', TRUE, 86.36),
+(4, '2024-11-07', FALSE, 86.35);
+
+-- insert sample data in CSR table
+INSERT INTO Customer_Seller_Relationships (customerID, sellerID) VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4);
+
+-- insert sample data in Line_Items table
 INSERT INTO Line_Items (orderID, productID, sell_price, qty) VALUES
 (1, 1, 119.99, 2),
 (2, 2, 49.99, 3),
