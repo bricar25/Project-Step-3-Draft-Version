1c923803ffca3ff46db652fd2077edf348aee7f7
usanro2151 <usanro2151.ds@gmail.com>
2024-11-07 10:48:08 -0600

    added to DML

--- DML.sql
+++ DML.sql
@@ -3,8 +3,7 @@
 -- functionalities listed in the Project Specs.
 
 
--- ***** customers and editCustomers pages ***** 
-
+-- ***** ***** ***** ***** customers and editCustomers pages ***** ***** ***** *****
 -- get a list of customers for customers main page
 select * from Customers; 
 
@@ -12,8 +11,7 @@ select * from Customers;
 select customerID, first_name, last_name, email, password, phone_number from Customers 
 where customerID = :customerID_selected_from_customers_page; 
 
--- *****  sellers and editSeller pages ***** 
-
+-- ***** ***** ***** *****  sellers and editSeller pages ***** ***** ***** *****
 -- get a list of sellers for sellers main page
 select sellerID, seller_first_name, seller_last_name, store_name, store_rating, follower_count from Sellers;
 
@@ -21,11 +19,18 @@ select sellerID, seller_first_name, seller_last_name, store_name, store_rating, 
 select sellerID, seller_first_name, seller_last_name, store_name, store_rating, follower_count from Sellers
 where sellerID = :sellerID_selected_from_sellers_page;
 
--- *****  CRMSelection, storeX_CRM, ?editCustomer? ***** 
+-- *****  ***** ***** ***** CRMSelection, storeX_CRM, ?editCustomer? ***** ***** ***** *****
+-- for store dropdown on CRMSelection
+select store_name from Sellers;
+
+-- get customers after user selects which CRM (which seller's CRM) to view
+select first_name, last_name from Customer_Seller_Relationships 
+    inner join Customers on Customer_Seller_Relationships.customerID = Customers.customerID
+    inner join Sellers on Customer_Seller_Relationships.sellerID = Sellers.sellerID
+    where Sellers.store_name = :store_selected_from_CRMSelection;
+
+-- ***** ***** ***** ***** ORDERS PAGES ***** ***** ***** *****
 
 
--- ORDERS PAGES
 
-
-
--- PRODUCTS PAGES
+-- ***** ***** ***** ***** PRODUCTS PAGES ***** ***** ***** *****
