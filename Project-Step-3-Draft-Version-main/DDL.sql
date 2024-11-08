a48679a291ca204c35dabd01532bdd5284e837e0
bricar25 <130022676+bricar25@users.noreply.github.com>
2024-11-07 20:40:15 -0800

    Add files via upload
    re-uploading deleted files

---
+++ Project-Step-3-Draft-Version-main/DDL.sql
@@ -0,0 +1,111 @@
+-- CREATE TABLES
+
+-- create Customers table
+CREATE OR REPLACE TABLE Customers (
+    customerID INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(32) NOT NULL,
+    last_name VARCHAR(32) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(32) NOT NULL,  
+    phone_number VARCHAR(12)
+);
+
+-- create Sellers table
+CREATE OR REPLACE TABLE Sellers (
+    sellerID INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    seller_first_name VARCHAR(32) NOT NULL,
+    seller_last_name VARCHAR(32) NOT NULL,
+    store_name VARCHAR(32) NOT NULL,
+    store_rating DECIMAL(3,1),
+    follower_count INT
+);
+
+-- create Products table
+CREATE OR REPLACE TABLE Products (
+    productID INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    sellerID INT NOT NULL, 
+    category VARCHAR(32) NOT NULL,
+    brand VARCHAR(32),
+    size VARCHAR(32),
+    price DECIMAL(6,2) NOT NULL,
+    product_condition VARCHAR(32) NOT NULL,
+    color VARCHAR(32),
+    FOREIGN KEY (sellerID) REFERENCES Sellers(sellerID)
+);
+
+-- create Orders table
+CREATE OR REPLACE TABLE Orders (
+    orderID INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    customerID INT NOT NULL,
+    order_date DATE NOT NULL,
+    shipped BOOLEAN DEFAULT FALSE,
+    total DECIMAL(6,2) NOT NULL,
+    FOREIGN KEY (customerID) REFERENCES Customers(customerID)
+);
+
+-- create Customer_Seller_Relationships table. intersection table between Customers and Sellers
+CREATE OR REPLACE TABLE Customer_Seller_Relationships (
+    csrID INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    customerID INT NOT NULL,
+    sellerID INT NOT NULL,
+    email_opt_out BOOLEAN DEFAULT TRUE,
+    FOREIGN KEY (customerID) REFERENCES Customers(customerID),
+    FOREIGN KEY (sellerID) REFERENCES Sellers(sellerID)
+);
+
+-- create Line_Items table. intersection table between Orders and Products
+CREATE OR REPLACE TABLE Line_Items (
+    lineitemID INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    orderID INT NOT NULL,
+    productID INT NOT NULL,
+    sell_price DECIMAL(6,2) NOT NULL,
+    qty INT NOT NULL,
+    UNIQUE (orderID, productID),  
+    FOREIGN KEY (orderID) REFERENCES Orders(orderID),
+    FOREIGN KEY (productID) REFERENCES Products(productID)
+);
+
+-- insert sample data into Customers table
+INSERT INTO Customers (first_name, last_name, email, password, phone_number) VALUES
+('Jamie', 'Dillan', 'jdill@yahoo.com', '123pasS!', '223-505-1234'),
+('Jane', 'Smith', 'janesmith@yahoo.com', 'Pass!word2', '425-505-6678'),
+('Alice', 'James', 'alicej@gmail.com', '2p0a2s4s?', '334-758-8765'),
+('Bobby', 'Wilde', 'bwilde@gmail.com', 'beWure!', '202-577-4321');
+
+-- insert sample data into Sellers table
+INSERT INTO Sellers (seller_first_name, seller_last_name, store_name, store_rating, follower_count) VALUES
+('Pam', 'Brell', 'Winner Tech Shop', 2.8, 1023),
+('Jon', 'Wimber', 'Wimber Wear', 4.5, 850),
+('Ella', 'Garcia', 'Crate and Home', 4.9, 670),
+('Sonny', 'Smith', 'Son Shades', 3.7, 1540);
+
+
+-- insert sample data in Products table
+INSERT INTO Products (sellerID, category, brand, size, price, product_condition, color) VALUES
+(1, 'Shirts', 'Adidas', 'Medium', 119.99, 'New', 'Black'),
+(2, 'Pants', 'Nike', 'Large', 49.99, 'New', 'Red'),
+(3, 'Pants', 'Arizona', 'Large', 19.99, 'Used', 'White'),
+(4, 'Accessories', 'Eddie Bauer', 'Small', 15.99, 'Used', 'Blue');
+
+-- insert sample data in Orders table
+INSERT INTO Orders (customerID, order_date, shipped, total) VALUES
+(1, '2023-08-06', TRUE, 259.18),
+(2, '2022-11-03', TRUE, 161.97),
+(3, '2021-10-01', TRUE, 86.36),
+(4, '2024-11-07', FALSE, 86.35);
+
+-- insert sample data in CSR table
+INSERT INTO Customer_Seller_Relationships (customerID, sellerID) VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4);
+
+-- insert sample data in Line_Items table
+INSERT INTO Line_Items (orderID, productID, sell_price, qty) VALUES
+(1, 1, 119.99, 2),
+(2, 2, 49.99, 3),
+(3, 3, 19.99, 4),
+(4, 4, 15.99, 5);
+
+
---
+++ Project-Step-3-Draft-Version-main/DML.sql
@@ -0,0 +1,159 @@
+-- These are some Database Manipulation queries for a partially implemented Project Website 
+-- Your submission should contain ALL the queries required to implement ALL the
+-- functionalities listed in the Project Specs.
+
+
+
+-- ***** ***** ***** ***** ***** customers and editCustomers pages ***** ***** ***** ***** *****
+-- get a list of customers for customers main page
+select customerID, first_name, last_name, email, password, phone_number from Customers; 
+
+-- get a single customer's data for the Edit Customer Profile form
+select customerID, first_name, last_name, email, password, phone_number from Customers 
+where customerID = :customerID_selected_from_customers_page; 
+
+-- update customer 
+update Customers set 
+    first_name   = :first_name_input, 
+    last_name    = :last_name_input,
+    email        = :email_input,
+    password     = :password_input,
+    phone_number = :phone_number_input
+where customerID = :customerID_selected_from_customers_page;
+
+-- add a new customer profile
+insert into Customers (first_name, last_name, email, password, phone_number)
+values (:first_name_input, :last_name_input, :email_input, :password_input, :phone_number_input);
+
+-- delete customer profile
+delete from Customers where customerID = :customerID_selected_from_customers_page;
+
+
+-- ***** ***** ***** ***** ***** sellers and editSeller pages ***** ***** ***** ***** *****
+-- get a list of sellers for sellers main page
+select sellerID, seller_first_name, seller_last_name, store_name, store_rating, follower_count from Sellers;
+
+-- get a single seller's data for the Edit Seller Profile form
+select sellerID, seller_first_name, seller_last_name, store_name, store_rating, follower_count from Sellers
+where sellerID = :sellerID_selected_from_sellers_page;
+
+-- update seller
+update Sellers set
+    seller_first_name = :seller_first_name_input,
+    seller_last_name  = :seller_first_name_input,
+    store_name        = :store_name_input,
+    store_rating      = :store_rating_input,
+    follower_count    = :follower_count_input,
+where sellerID = :sellerID_selected_from_sellers_page;
+
+-- add a new seller profile
+insert into Sellers (seller_first_name, seller_last_name, store_name, store_rating, follower_count)
+values (:seller_first_name_input, :seller_last_name_input, :store_name_input, :store_rating_input, :follower_count_input);
+
+-- delete seller profile
+delete from Sellers where sellerID = :sellerID_selected_from_sellers_page;
+
+
+--  ***** ***** ***** ***** ***** CRMSelection, storeX_CRM, ?editCustomer? ***** ***** ***** ***** *****
+-- for store dropdown on CRMSelection
+select store_name from Sellers;
+
+-- get customers after user selects which CRM (which seller's CRM) to view
+select first_name, last_name from Customer_Seller_Relationships 
+    inner join Customers on Customer_Seller_Relationships.customerID = Customers.customerID
+    inner join Sellers on Customer_Seller_Relationships.sellerID = Sellers.sellerID
+    where Sellers.store_name = :store_selected_from_CRMSelection;
+
+-- get a single CSR data for the Edit Seller Profile form
+
+-- update store's CRM by updating 
+update Customer_Seller_Relationships set
+    email_opt_out = :email_opt_out_input
+where csrID = (
+    select csrID from Customer_Seller_Relationships 
+    inner join Customers on Customer_Seller_Relationships.customerID = Customers.customerID
+    inner join Sellers on Customer_Seller_Relationships.sellerID = Sellers.sellerID
+    where Sellers.store_name = :store_selected_from_CRMSelection and Customers.customerID = :customerID_selected_from_CRM);
+
+-- delete customer tracking from store's CRM 
+delete from Customer_Seller_Relationships 
+where csrID = (
+    select csrID from Customer_Seller_Relationships 
+    inner join Customers on Customer_Seller_Relationships.customerID = Customers.customerID
+    inner join Sellers on Customer_Seller_Relationships.sellerID = Sellers.sellerID
+    where Sellers.store_name = :store_selected_from_CRMSelection and Customers.customerID = :customerID_selected_from_CRM
+);
+
+
+--  ***** ***** ***** ***** ***** ORDERS PAGES ***** ***** ***** ***** *****
+-- get a list of orders for the orders main page
+select orderID, customerID, order_date, shipped, total from Orders;
+
+-- get a single order's data for the Edit Order page
+select orderID, customerID, order_date, shipped, total from Orders
+where orderID = :orderID_selected_from_orders_page;
+
+-- update order information
+update Orders set
+    customerID = :customerID_input,
+    order_date = :order_date_input,
+    shipped    = :shipped_input,
+    total      = :total_input
+where orderID = :orderID_selected_from_orders_page;
+
+-- add a new order
+insert into Orders (customerID, order_date, shipped, total)
+values (:customerID_input, :order_date_input, :shipped_input, :total_input);
+
+-- delete an order
+delete from Orders where orderID = :orderID_selected_from_orders_page;
+
+--  ***** ***** ***** ***** ***** PRODUCTS PAGES ***** ***** ***** ***** *****
+-- get a list of products for the products main page
+select productID, sellerID, category, brand, size, price, product_condition, color from Products;
+
+-- get a single product's data for the Edit Product page
+select productID, sellerID, category, brand, size, price, product_condition, color from Products
+where productID = :productID_selected_from_products_page;
+
+-- update product information
+update Products set
+    sellerID          = :sellerID_input,
+    category          = :category_input,
+    brand             = :brand_input,
+    size              = :size_input,
+    price             = :price_input,
+    product_condition = :product_condition_input,
+    color             = :color_input
+where productID = :productID_selected_from_products_page;
+
+-- add a new product
+insert into Products (sellerID, category, brand, size, price, product_condition, color)
+values (:sellerID_input, :category_input, :brand_input, :size_input, :price_input, :product_condition_input, :color_input);
+
+-- delete a product
+delete from Products where productID = :productID_selected_from_products_page;
+
+--  ***** ***** ***** ***** ***** LINE ITEMS PAGES ***** ***** ***** ***** *****
+-- get a list of line items for a specific order on the line items page
+select lineitemID, orderID, productID, sell_price, qty from Line_Items
+where orderID = :orderID_selected_from_orders_page;
+
+-- get a single line item data for editing
+select lineitemID, orderID, productID, sell_price, qty from Line_Items
+where lineitemID = :lineitemID_selected_from_line_items;
+
+-- update line item information
+update Line_Items set
+    orderID    = :orderID_input,
+    productID  = :productID_input,
+    sell_price = :sell_price_input,
+    qty        = :qty_input
+where lineitemID = :lineitemID_selected_from_line_items;
+
+-- add a new line item
+insert into Line_Items (orderID, productID, sell_price, qty)
+values (:orderID_input, :productID_input, :sell_price_input, :qty_input);
+
+-- delete a line item
+delete from Line_Items where lineitemID = :lineitemID_selected_from_line_items;
